9aeba67e13b8ccd6be29495b32556b9892ee212c
Aaron Sys <aaron.sys@student.hogent.be>
2020-02-17 15:23:27 +0100

    #2 Databank properties toewijzen

--- dotnet-g033/Data/Mappers/SessieConfig.cs
+++ dotnet-g033/Data/Mappers/SessieConfig.cs
@@ -13,8 +13,14 @@ namespace dotnet_g033.Data.Mappers
         public void Configure(EntityTypeBuilder<Sessie> builder)
         {
             builder.ToTable("Sessie");
-            builder.Property(s => s.Id).IsRequired();
-            builder.Property(s => s.Naam).HasMaxLength(100).IsRequired();
+            builder.HasKey(s => s.Id);
+
+            //builder.Property(s => s.Id).IsRequired();
+            builder.Property(s => s.Naam).HasMaxLength(50).IsRequired();
+            builder.Property(d => d.StartDatum).IsRequired();
+            builder.Property(d => d.EindDatum).IsRequired();
+            builder.Property(m => m.MaxCap).HasDefaultValue(30).IsRequired();
+            builder.Property(m => m.AantalAanwezigeGebruikers).HasDefaultValue(1);
 
         }
     }
