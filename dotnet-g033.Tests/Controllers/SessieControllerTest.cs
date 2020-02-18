6a7b983935a2ebe1662923ed044c08905b9d71b8
Simon Bettens <simon.bettens@student.hogent.be>
2020-02-18 19:13:28 +0100

    #12 Testen toegevoegd

---
+++ dotnet-g033.Tests/Controllers/SessieControllerTest.cs
@@ -0,0 +1,72 @@
+﻿using dotnet_g033.Controllers;
+using dotnet_g033.Models.Domain;
+using dotnet_g033.Tests.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace dotnet_g033.Tests.Controllers
+{
+    
+   
+    public class SessieControllerTest
+    {
+        private readonly DummyApplicationDbContext _dummyContext;
+        private readonly SessieController _sessieController;
+        private readonly Mock<ISessieRepository> _mockSessieRepository;
+        private readonly int _maandJan = 1;
+        private readonly int _maandFeb = 2;
+        private readonly int _huidigeMaand;
+
+        public SessieControllerTest()
+        {
+            _dummyContext = new DummyApplicationDbContext();
+            _mockSessieRepository = new Mock<ISessieRepository>();
+            _sessieController = new SessieController(_mockSessieRepository.Object)
+            {
+                TempData = new Mock<ITempDataDictionary>().Object
+            };
+            _huidigeMaand = DateTime.Now.Month;
+        }
+
+        #region Index
+        [Fact]
+        public void IndexGeeftLijstTerugMetEnkelSessiesVanDeMaandFebruari() {
+            _mockSessieRepository.Setup(s => s.GetByMaand(_maandFeb)).Returns(_dummyContext.SessiesFeb);
+
+            var result = Assert.IsType<ViewResult>(_sessieController.Index(_maandFeb));
+            List<Sessie> sessies = Assert.IsType<List<Sessie>>(result.Model);
+            var maanden = Assert.IsType<SelectList>(result.ViewData["Maanden"]);
+            Assert.Equal(_maandFeb.ToString(), maanden.SelectedValue.ToString());
+            Assert.Equal(4, sessies.Count);
+        }
+        [Fact]
+        public void IndexGeeftLijstTerugMetEnkelSessiesVanDeHuidigeMaand()
+        {
+            _mockSessieRepository.Setup(s => s.GetByMaand(0)).Returns(_dummyContext.SessiesFeb);
+            var result = Assert.IsType<ViewResult>(_sessieController.Index(0));
+            List<Sessie> sessies = Assert.IsType<List<Sessie>>(result.Model);
+            var maanden = Assert.IsType<SelectList>(result.ViewData["Maanden"]);
+            Assert.Equal(_huidigeMaand.ToString(), maanden.SelectedValue.ToString());
+        }
+        [Fact]
+        public void IndexGeeftLegeLijstTerugVanMaandJanuari()
+        {
+            _mockSessieRepository.Setup(s => s.GetByMaand(_maandJan)).Returns(new List<Sessie>());
+            var result = Assert.IsType<ViewResult>(_sessieController.Index(_maandJan));
+            List<Sessie> sessies = Assert.IsType<List<Sessie>>(result.Model);
+            var maanden = Assert.IsType<SelectList>(result.ViewData["Maanden"]);
+            Assert.Equal(_maandJan.ToString(), maanden.SelectedValue.ToString());
+            Assert.Empty(sessies);
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ dotnet-g033.Tests/Data/DummyApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿
+using dotnet_g033.Models.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace dotnet_g033.Tests.Data
+{
+    public class DummyApplicationDbContext
+    {
+        private readonly IList<Sessie> _sessies;
+        private readonly IList<Sessie> _sessiesFeb;
+
+        public Sessie Sessie1 { get; set; }
+        public Sessie Sessie2 { get; set; }
+        public Sessie Sessie3 { get; set; }
+        public Sessie Sessie4 { get; set; }
+        public Sessie Sessie5 { get; set; }
+        public IEnumerable<Sessie> Sessies => _sessies;
+        public IEnumerable<Sessie> SessiesFeb => _sessiesFeb;
+        public DummyApplicationDbContext()
+        {
+            Sessie1 = new Sessie("sessie1", new DateTime(2020, 2, 20, 14, 0, 0), new DateTime(2020, 2, 20, 15, 0, 0), false, 20, 0);
+            Sessie2 = new Sessie("sessie2", new DateTime(2020, 2, 21, 10, 30, 0), new DateTime(2020, 2, 21, 12, 30, 0), false, 10, 0);
+            Sessie3 = new Sessie("sessie3", new DateTime(2020, 2, 22, 16, 0, 0), new DateTime(2020, 2, 22, 17, 0, 0), true, 30, 0);
+            Sessie4 = new Sessie("sessie4", new DateTime(2020, 2, 19, 14, 0, 0), new DateTime(2020, 2, 19, 15, 0, 0), false, 20, 0);
+            Sessie5 = new Sessie("sessie5", new DateTime(2020, 3, 19, 14, 0, 0), new DateTime(2020, 2, 19, 15, 0, 0), false, 20, 0);
+            _sessies = new List<Sessie> { Sessie1, Sessie2, Sessie3, Sessie4, Sessie5 };
+            _sessiesFeb = new List<Sessie> { Sessie1, Sessie2, Sessie3, Sessie4};
+        }
+
+    }
+}
