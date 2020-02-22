92f32dd70867c97005cd4959fca7733528d24d96
Ruben Naudts <ruben.naudts@student.hogent.be>
2020-02-22 13:26:54 +0100

    #3 details opnieuw gedaan met viewmodel ipv viewdata

--- dotnet-g033/Controllers/SessieController.cs
+++ dotnet-g033/Controllers/SessieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using dotnet_g033.Models.Domain;
+using dotnet_g033.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -26,8 +27,9 @@ namespace dotnet_g033.Controllers
         }
 
         public ActionResult Details(int id) {
-            ViewData["Sessie"] = _sessieRepository.GetById(id);
-            return View();
+            Sessie sessie = _sessieRepository.GetById(id);
+            var viewModel = new SessieDetailsViewModel(sessie);
+            return View(viewModel);
         }
         private SelectList GetMaandSelectList(int maandId = 0)
         {
--- dotnet-g033/Models/ViewModels/SessieDetailsViewModel.cs
+++ dotnet-g033/Models/ViewModels/SessieDetailsViewModel.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using dotnet_g033.Models.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace dotnet_g033.Models.ViewModels {
     public class SessieDetailsViewModel {
-            
+        public SessieDetailsViewModel(Sessie sessie) {
+            this.Sessie = sessie;
+        }
+
+        /*public String Naam { get; set; }
+public String Lokaal { get; set; }
+public DateTime StartDatum { get; set; }
+public DateTime EindDatum { get; set; }
+public int AantalAanwezigeGebruikers { get; set; }
+public int MaxCap { get; set; }*/
+        public Sessie Sessie { get; set; }
     }
 }
