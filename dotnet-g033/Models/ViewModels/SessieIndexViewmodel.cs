145bddbca0411573315608ea36716288285e5763
Aaron Sys <aaron.sys@student.hogent.be>
2020-03-02 15:10:29 +0100

    #9, open_sessies zichtbaarheid

---
+++ dotnet-g033/Models/ViewModels/SessieIndexViewmodel.cs
@@ -0,0 +1,18 @@
+﻿using dotnet_g033.Models.Domain;
+using System.Collections.Generic;
+
+namespace dotnet_g033.Models.ViewModels
+{
+    public class SessieIndexViewmodel
+    {
+        public Gebruiker Gebruiker { get; set; }
+
+        public IEnumerable<Sessie> Sessies  { get; set; }
+        public SessieIndexViewmodel(Gebruiker gebruiker, IEnumerable<Sessie> sessies) 
+        {
+            this.Gebruiker = gebruiker;
+            this.Sessies = sessies;
+        }
+
+    }
+}
